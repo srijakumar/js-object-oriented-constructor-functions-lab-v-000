ab406c24e06aea9084489dc462462de57a0a765d
Srija S <srijakumar7@gmail.com>
2018-02-11 23:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function Scooter(year, color, model){
   this.color = color;
   this.model = model;
 }
+
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function Scooter(year, color, model){
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
